9f9b50005b4157634ebfa76b65911d651d00e116
timothycobrien <timothy.c.obrien99@gmail.com>
2019-09-18 18:31:00 -0500

    Initial commit with a few changes so far

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'timcobrien.tech',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Tim O\'Brien',
+  subtitle: 'Get to know me!',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -12,8 +12,12 @@ module.exports = {
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'Home',
       path: '/'
+    },
+    {
+      label: 'Resume',
+      path: '/pages/resume'
     },
     {
       label: 'About me',
@@ -25,22 +29,22 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Tim O\'Brien',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'I\'m a junior at Vanderbilt University studying Computer Science, Applied Mathematics, and French. Please check out my projects, resume, or blog (in-progress). If you have any questions or just want to learn more, feel free to get in touch!',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 'timothy.c.obrien@vanderbilt.edu',
+      facebook: '',
+      telegram: '',
+      twitter: '',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      instagram: '',
       line: '',
       gitlab: '',
-      weibo: ''
+      weibo: '',
+      github: 'https://github.com/timothycobrien',
+      linkedin: 'https://www.linkedin.com/in/timothyc-obrien/',
     }
   }
 };
